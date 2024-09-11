f1b55e13714c309aad2d56855e85e840b4da3524
thatanoslord <sabbarmoussa@gmail.com>
2024-09-12 00:56:54 +0100

    birth calcul plus optimisation of code

---
+++ octocode/birth_calcul.py
@@ -0,0 +1,2 @@
+age = int(input("Please type your age here: "))
+print(f"your born in the year of: {2024-age}")
--- octocode/calcul.py
+++ octocode/calcul.py
@@ -4,9 +4,8 @@
 # print("The total area is: ", (float(length) + float(width))*2)
 
 #THIS IS THE SECOND PROJECT
-length2 = input("Please type Length: \n")
-width2 = input("Please type Width: \n")
-space = (float(width2) + float(length2))*2
-price = input("How much for 1 meter? \n")
-print("The total area is: ",space)
-print("Gve the worker: ", space * float(price))
+length2 = float(input("Please type Length: \n"))
+width2 = float(input("Please type Width: \n"))
+price =float(input("How much for 1 meter? \n"))
+print(f"The total area is: {float((length2+width2)*2)}")
+print(f"GIve the worker: {(length2+width2)*2*price}")
