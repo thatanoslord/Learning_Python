bf87cf56bade5187be13a5beb7b0e9ae92744eb8
thatanoslord <sabbarmoussa@gmail.com>
2024-09-12 00:05:13 +0100

    calcul func

---
+++ octocode/calcul.py
@@ -0,0 +1,12 @@
+#THIS THE first project of calcul
+# length = input("Please type length: \n")
+# width = input("Please type width: \n")
+# print("The total area is: ", (float(length) + float(width))*2)
+
+#THIS IS THE SECOND PROJECT
+length2 = input("Please type Length: \n")
+width2 = input("Please type Width: \n")
+space = (float(width2) + float(length2))*2
+price = input("How much for 1 meter? \n")
+print("The total area is: ",space)
+print("Gve the worker: ", space * float(price))
