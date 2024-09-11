36b5b8c54d7f25d7fc9ae920a1d41185d8e797ca
thatanoslord <sabbarmoussa@gmail.com>
2024-09-11 22:08:17 +0100

    second func

---
+++ octocode/func1.py
@@ -0,0 +1,6 @@
+# in this function we will focus on understanding how does len() work
+# and type () of input value 
+pin = input("Enter you pin here: \n")
+print("your Pin length is: ", len(pin))
+print("type of input value: ", type(pin))
+
