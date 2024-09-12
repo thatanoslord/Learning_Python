c20fd45b9966cf09709cde8a07eb45c9e2da4a96
thatanoslord <sabbarmoussa@gmail.com>
2024-09-12 16:29:04 +0100

    if condition file

---
+++ octocode/if_else.py
@@ -0,0 +1,8 @@
+print("Welcome to my application: ")
+age = int(input("How old are you?\n"))
+#python call this if condition "Code block" 
+if age >= 12:
+    print("good you can use the app")
+else:
+    print("Sorry, you can't use the app")
+#Because of the code block we call python indent language
