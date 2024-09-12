a3c349b2e0cde7f24f5f564877c74cd99e7f3bfc
thatanoslord <sabbarmoussa@gmail.com>
2024-09-12 16:45:36 +0100

    elif

---
+++ octocode/if_elif_else.py
@@ -0,0 +1,10 @@
+print("this program will show your your grade:")
+grade = float(input("type you mark: \n"))
+if grade >= 90:
+    print("you got: an A mark")
+elif grade >=75:
+    print("you got: a B mark")
+elif grade == 50:
+    print("you got: a C mark")
+else:
+    print("you got: an F mark")
