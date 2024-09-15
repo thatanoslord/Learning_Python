4013bbff2917d20c358e6243cbc263f4a746f613
thatanoslord <sabbarmoussa@gmail.com>
2024-09-15 01:47:49 +0100

    nested if

--- octocode/nested_if.py
+++ octocode/nested_if.py
@@ -1,7 +1,13 @@
-nationality = input("are you Moroccan? print Yes or No\n"  )
+nationality = input("are you Moroccan? print 'Yes' or 'No'\n"  ).lower()
 if nationality.lower() == "yes":
-    if nationality.lower() == "no":
-        print("sorry you are not allowed to have Moroccan nationality")
-        age = int(input("How old are you ?\n"))
-        if age >= 18:
-            print("you can get you Id card")
+    print("good, that's the first step")
+    
+    is_18 = input("are you 18 or above? 'Yes' or 'No' : ").lower()
+    if is_18 == "yes":
+        print("You apply for your ID.")
+    elif is_18 == "no":
+        print("Sorry, you can't have an ID because you are not old enough.")
+    else:
+        print(f"Sorry, '{is_18}' isn't in the choices you have to write either 'Yes' or 'No'")
+else:
+    print("Sorry, because you are not Moroccan you can't apply for the ID.")
