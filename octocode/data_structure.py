04efb93f5ab1c34866cf297cbfb68ad140ad0d47
thatanoslord <sabbarmoussa@gmail.com>
2024-09-17 15:19:19 +0100

    data structure beginning

---
+++ octocode/data_structure.py
@@ -0,0 +1,5 @@
+print("trying to learn what is Data structure:")
+bestfriends = ["Habib", "Kamal", "Hasnae", "Joker", "Youness"]
+print(f"the first best friend in my list is {bestfriends[0]} and the last bestfriend in my list is {bestfriends[-1]} ")
+bestfriends[-2] = "Hicham"
+print(f"{bestfriends[-2]} is the best barber in the town")
