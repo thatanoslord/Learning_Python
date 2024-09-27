23ccf974f168d88c041fd4ce92da9442d1a7ece0
thatanoslord <sabbarmoussa@gmail.com>
2024-09-27 20:32:56 +0100

    rabbit game

--- octocode/place_the_rabbit.py
+++ octocode/place_the_rabbit.py
@@ -1,6 +1,6 @@
 #entrance to the game 
 print("Welcome to place the rabbit")
-place = [["🌱", "🌱", "🌱"],["🌱", "🌱", "🌱"],["🌱", "🌱", "🌱"]]
+place = [["🌱", "🌱", "🌱","🌱"],["🌱", "🌱", "🌱","🌱"],["🌱", "🌱", "🌱","🌱"] ,["🌱", "🌱", "🌱","🌱"] ]
 print(f"""
       {place[0]}
       {place[1]}
@@ -13,12 +13,14 @@ position = input("please enter row and column so the rabbit can get the carrots:
 #indexing the position of the input
 row = int(position[0])
 column = int(position[1])
-
+if row >= 1 and row < 5 and  column>= 1 and column < 5:
 #using the indexing inside the place so can be changer to the rabbit
-place[row - 1][column -1] = '🐇' 
-
-print(f"""
+    place[row - 1][column -1] = '🐇' 
+    print(f"""
       {place[0]}
       {place[1]}
       {place[2]}
+      {place[3]}
       """)
+else:
+    print("the number is false")
