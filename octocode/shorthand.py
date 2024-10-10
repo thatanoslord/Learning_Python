9a8762ed819747f82f41f539cceee62bd33ac663
thatanoslord <sabbarmoussa@gmail.com>
2024-10-10 15:42:31 +0100

    oprimized

--- octocode/shorthand.py
+++ octocode/shorthand.py
@@ -1,8 +1,7 @@
 numbers= [1,2,3,4,5]
 total=0
 print("let's sum the numbers from 1 to 5")
-for i in range(len(numbers)):
-    print(f"-> {i+1}")
-    total += numbers[i]
-    print(f"current Total: {total}")
-print(f"total: {total} \n")
+for i in numbers:
+    print(f"->{i}")
+    total+=i
+    print(f"total is : {total}")
